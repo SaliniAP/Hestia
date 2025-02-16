9efa100a0ab663b43b969cfea1f86da16d2b2137
SaliniAP <spunchiw@uwaterloo.ca>
2025-02-16 01:06:48 -0500

    Completed journal.py

--- Home.py
+++ Home.py
@@ -20,12 +20,6 @@ if "visibility" not in st.session_state:
 if "tasks" not in st.session_state:
     st.session_state.tasks = []
 
-if "journal_entries" not in st.session_state:
-    st.session_state.journal_entries = []
-
-if "journal_page" not in st.session_state:
-    st.session_state.journal_page = "collapsed"
-
 
 
 def toggle_sidebar_todo():
@@ -34,33 +28,10 @@ def toggle_sidebar_todo():
 def toggle_sidebar_journal():
     st.session_state.journal = not st.session_state.journal
 
-def save_entry(entry_text):
-    if entry_text:
-        st.toast("Journal entry saved!")
-        entry = {
-            "date": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-            "text": entry_text,
-        }
-
-        st.session_state.journal_entries.append(entry)
-        st.success("Journal entry saved!")
-        st.sidebar.success("Go to 'journal' to see your entries.")
-
-def new_journal():
-    with st.container():
-        entry_text = st.text_area("Write your journal entry:")
-        st.write(f"You wrote {len(entry_text)} characters.")
-        st.button("Save Entry", help="Save your entry", on_click=save_entry(entry_text))
-    
 
 # Sidebar content (only shown when sidebar_state is True)
-left, right = st.columns(2)
-with left:
-    if st.button("To-Do List", help = "Open your daily to-do list"):
-        toggle_sidebar_todo()
-with right:
-    if st.button("Journal Entry", help = "Open your journal"):
-        toggle_sidebar_journal()
+if st.button("To-Do List", help = "Open your daily to-do list"):
+    toggle_sidebar_todo()
 
 if st.session_state.sidebar_state:
     with st.sidebar:
@@ -69,7 +40,7 @@ if st.session_state.sidebar_state:
         
         
         text_input = st.text_input(
-            "Enter some text 👇",
+            "What's something on your mind 👇",
             label_visibility=st.session_state.visibility,
             disabled=st.session_state.disabled,
             placeholder="Enter an item here",
@@ -88,21 +59,13 @@ if st.session_state.sidebar_state:
                 # Update task status
                 st.session_state.tasks[index]["completed"] = completed
             # Button to remove completed tasks
-            if st.button("Remove Completed Tasks"):
-                st.session_state.tasks = [task for task in st.session_state.tasks if not task["completed"]]
-                st.rerun()
+        if st.button("Remove Completed Tasks"):
+            st.session_state.tasks = [task for task in st.session_state.tasks if not task["completed"]]
+            st.rerun()
             
         
         st.button("Close To-Do List", on_click=toggle_sidebar_todo)
-    
-if st.session_state.journal:
-    with st.sidebar:
-        st.write("### Journal")
-        st.write("This is your journal. You can add journal entries by pressing New Entry. More entries will allow your planet to thrive!")
-        
-        st.button("New Entry", on_click=new_journal)
-        #st.rerun()
-        st.button("Close Journal", on_click=toggle_sidebar_journal)
+
 
 ## Planet health
 st.write("Your planet's health")
